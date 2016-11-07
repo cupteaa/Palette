38036d50bedad0adbd7107260c77d7aa40a43bfd
cupteaa <yu_yawei87@126.com>
2016-11-08 07:55:03 +0800

    初始化项目

---
+++ Palette/Palette/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Palette
+//
+//  Created by 于亚伟 on 2016/11/8.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Palette/Palette/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Palette
+//
+//  Created by 于亚伟 on 2016/11/8.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
